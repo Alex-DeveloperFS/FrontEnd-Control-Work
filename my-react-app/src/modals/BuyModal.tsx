8f3389019cfe6c39c88dadf17b0dc307733a3757
Alex-DeveloperFS <iotov.developer@gmail.com>
2024-09-01 12:16:54 +0300

    сделал покупку через кнопку (гuser) commit

---
+++ my-react-app/src/modals/BuyModal.tsx
@@ -0,0 +1,28 @@
+import {ReactNode, MouseEvent} from "react"
+import {createPortal} from "react-dom"
+
+interface ModalPropsInterface {
+  children: ReactNode,
+  onClose: () => void
+}
+
+const modalRoot = document.getElementById('modal-root')
+
+const Modal = ({children, onClose}: ModalPropsInterface) => {
+  if (!modalRoot) return null
+
+  const handleContentClick = (e: MouseEvent) => e.stopPropagation()
+
+  return createPortal(
+    <div className="modal-overlay" onClick={onClose}>
+      <div className="modal" onClick={handleContentClick}>
+        <span className="modal__close" onClick={onClose}>
+          x
+        </span>
+        {children}
+      </div>
+    </div>,
+    modalRoot
+  )
+}
+export default Modal
